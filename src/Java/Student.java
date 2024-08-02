061ff00c32a66b26b73db57621f360fa003f5c44
Ashwamedh Arote <ashwamedh.arote@gmail.com>
2024-08-02 11:41:39 +0530

    Initial Commit

---
+++ src/Java/Student.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am a Student !!!");
+
+	}
+
+}
